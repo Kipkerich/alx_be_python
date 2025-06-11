bec5d124d537576a9f241148c25e71bd5904d870
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-06-11 15:29:10 +0300

    Added the phrase  reminder and note on the if else statement

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -7,24 +7,24 @@ time_bound =input("Is it time-bound? (yes/no):")
 match Priority:
     case 'high':
         if time_bound == 'yes':
-            print(f" {Task} is a {Priority} priority that requires immediate attention  today!")
+            print(f"Reminder: {Task} is a {Priority} priority that requires immediate attention  today!")
         
         else:
-            print(f"{Task} is a {Priority} priority task. Consider completing it when you have free time.")
+            print(f"Note: {Task} is a {Priority} priority task. Consider completing it when you have free time.")
             
     case 'medium':
         if time_bound == 'yes':
-                 print(f" {Task} is a {Priority} priority that requires immediate attention  today!")
+                 print(f"Reminder: {Task} is a {Priority} priority that requires immediate attention  today!")
         
         else:
-            print(f"{Task} is a {Priority} priority task. Consider completing it when you have free time.")
+            print(f"Note: {Task} is a {Priority} priority task. Consider completing it when you have free time.")
             
     case 'low':
         if time_bound == 'yes':
-             print(f" {Task} is a {Priority} priority that requires immediate attention  today!")
+             print(f"Reminder: {Task} is a {Priority} priority that requires immediate attention  today!")
         
         else:
-            print(f"{Task} is a {Priority} priority task. Consider completing it when you have free time.")
+            print(f"Note: {Task} is a {Priority} priority task. Consider completing it when you have free time.")
             
     case _:
         print('Invalid task input')

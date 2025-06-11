4673c06a8855d1551108fbcd8d5cccdc4742ddd9
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-06-11 16:46:24 +0300

    Made some ammendments on the print statements

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -6,20 +6,21 @@ operation= input("Choose the operation (+, -, *, /):")
 match operation:
     case "+":
         result = num1 + num2
-        print (f"The result is {result}.")
+        
         
     case "-":
         result = num2 - num1
-        print (f"The result is {result}.")
+       
     case "*":
         result = num1 * num2
-        print (f"The result is {result}.")
+       
     case '/':
         if num2 == 0:
             print('Cannot be divided by zero')
         else:
             result = num1/ num2
-            print (f"The result is {result}.")
+            
     case _:
         print("No such operation")
-        
+    
+print (f'The result is {result}.')  

669868bb212375807e90819c56f01d134886ec3d
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-06-13 22:50:33 +0300

    Adding the explore datetime file

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -7,3 +7,12 @@ def display_current_datetime():
     return print(current_date)
 
 display_current_datetime()
+
+
+def calculate_future_date():
+    number_of_days = int(input('Enter the  number of days to add to the current date:'))
+    future_date =  timedelta(days=number_of_days)
+    
+    return print(future_date)
+
+calculate_future_date()

7e95501fcfb43287f153cf39c239c4ca36c1e45a
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-06-16 00:06:11 +0300

    Added the test simple caluculator function

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,6 +1,7 @@
 import unittest 
 import simple_calculator
 
-# class TestSimpleCalculator(unittest.TestCase):
+class TestSimpleCalculator(unittest.TestCase):
     
-#     def
+    def test_simple_calculator(self):
+        self.add(1,2, 3)

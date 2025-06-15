25fd4f84c787f65a52a963b6c5e5a9543680c9c3
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-06-15 22:47:30 +0300

    Added the programming paradigm folder

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount:
+    
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+        
+    def deposit(amount):
+            pass
+    def withdraw(self,amount):
+            pass
+        
+    def display_balance(self):
+            pass
+        
+
+        
+        

1d8deae630a8cf7820f2b6fd35934332653f2d34
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-06-15 22:53:11 +0300

    Added the default value for the account balance to be zero

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -2,13 +2,14 @@ class BankAccount:
     
     def __init__(self, account_balance):
         self.account_balance = account_balance
+        account_balance = 0
         
     def deposit(amount):
             pass
-    def withdraw(self,amount):
+    def withdraw(amount):
             pass
         
-    def display_balance(self):
+    def display_balance():
             pass
         
 

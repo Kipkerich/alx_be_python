b2ef68beddc2e1ef99df472bacca81c30f905cbc
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-06-19 14:18:09 +0300

    Added the library management file

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,11 @@
+class Book:
+    
+    def __init__(self, title, author, is_checked_out):
+        self.title = title
+        self.author = author
+        self.is_checked_out = is_checked_out
+        
+    
+    def library(self, book):
+        pass
+        

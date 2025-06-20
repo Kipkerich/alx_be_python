f3487b35b8a128a7dec8d739767d1a61931adc14
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-06-20 23:42:54 +0300

    added return value in the try and exception method

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -2,15 +2,20 @@ def safe_divide(numerator, denominator):
 
    
     try:
+      
         result = float(numerator) / float(denominator)
         if denominator == 0:
             raise ZeroDivisionError
         
     except ZeroDivisionError:
-        print('Error: Cannot divide by zero.')
+        return print('Error: Cannot divide by zero.')
         
     except ValueError:
-        print('Error: Please enter numeric values only.')
+        return print('Error: Please enter numeric values only.')
         
     else:
-        print(f"The result of the division is {float(result)}")
+         return print(f"The result of the division is {float(result)}")
+        
+
+
+safe_divide(10, 5)

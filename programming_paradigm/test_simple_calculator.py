473960f9877d08e0b07a73d676fc8c3ed2a2fd98
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-06-25 18:06:25 +0300

    set up the simple calculator instance before the subtraction test

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -11,3 +11,13 @@ class TestSimpleCalculator(unittest.TestCase):
         """Test the addition method."""
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
+        
+        
+        self.calc = SimpleCalculator()
+
+    def test_subtract(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(-1, 1), -2)
+        self.assertEqual(self.calc.subtract(1, -2) 3)
+        

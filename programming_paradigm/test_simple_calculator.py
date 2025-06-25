6a090d645ea4ad54843a7db2d159faba7d3c1157
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-06-25 18:13:36 +0300

    set up the simple calculator instance before the division test

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -29,3 +29,12 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(5, 3), 15)
         self.assertEqual(self.calc.multiply(-1, 1), -1)
         self.assertEqual(self.calc.multiply(1, -2), -2)
+        
+    
+        self.calc = SimpleCalculator()
+
+    def test_division(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.divide(6, 3), 2)
+        self.assertEqual(self.calc.divide(-8, 1), -8)
+        self.assertEqual(self.calc.divide(1, -2), -2)

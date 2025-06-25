8f8bf765a8fd92231aac9057c1c10d058013ef93
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-06-25 18:02:25 +0300

    set up the simple calculator instance before the test

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,16 +1,13 @@
-import simple_calculator
-import unittest 
-
+import unittest
+from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
-    
-    def test_simple_calculator(self):
-        self.assertEqual(simple_calculator.add(1, 3), 4)
-        self.assertEqual(simple_calculator.add(-1, 3), 2)
-        self.assertEqual(simple_calculator.add(1, -3), 2)
-        self.assertEqual(simple_calculator.add(-1, -3), -4)
-        
-        self.assertEqual(simple_calculator.subtract(1, 3), -2)
-        self.assertEqual(simple_calculator.subtract(-1, 3), -4)
-        self.assertEqual(simple_calculator.subtract(1, -3), 4)
-        self.assertEqual(simple_calculator.subtract(-1, -3), -2)
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)

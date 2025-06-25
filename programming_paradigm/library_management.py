01c28139a4a3367ef2d569b5c1bcedf096e8d55a
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-06-25 19:50:05 +0300

    added methods to library class

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,17 +1,29 @@
 class Book:
-    
-    __is_checked_out = None
-    
+ 
     def __init__(self, title, author, is_checked_out):
         self.title = title
         self.author = author
-        self.__is_checked_out = is_checked_out
+        self._is_checked_out = is_checked_out
         
     
 class Library(Book):
     
-    def __init__(self,title, is_checked_out):
+    def __init__(self,title, books):
         
-        super().__init__(title, is_checked_out)
+        super().__init__(title)
+        self._books = books
+        
+    def add_book():
+        Book = {}
+        return Book
+    
+    def check_out_book(title):
+        pass
+    
+    def return_book(title):
+        pass
+    
+    def list_available_books(title):
+        pass
         
         
